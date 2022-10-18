a0e3eed618716b3a5b58077115f0f153b7ccd812
Akinremi Isaiah Adebayo <akinremi93@gmail.com>
2022-10-18 14:46:12 -0700

    ok

--- _printf.c
+++ _printf.c
@@ -16,6 +16,7 @@ int _printf(const char * const format, ...)
 	};
 	va_list args;
 	int i = 0, j, len = 0;
+
 	va_start(args, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
